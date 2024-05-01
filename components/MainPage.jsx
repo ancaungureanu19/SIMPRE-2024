f506e769e85c917595892ab1ea39628d9a95d29d
Cristina Ungureanu <ungureanuanca19@stud.ase.ro>
2024-05-01 20:37:12 +0300

    Delete button

--- components/MainPage.jsx
+++ components/MainPage.jsx
@@ -1,44 +1,39 @@
-import { getRecords } from "@/utils/recordsFunction";
-import React, { useEffect, useState } from "react";
+import { getRecords, deleteRecord } from "@/utils/recordsFunction";
+import { useEffect, useState } from "react";
 import Spinner from "./Spinner";
 import { useRouter } from "next/router";
 
 
 const MainPage = () => {
   const router = useRouter();
-    const [data, setData] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
+  const [records, setRecords] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    const fetchRecords = async () => {
-        try {
-          const response = await getRecords();
-    
-          setData(response);
-          setIsLoading(false);
-        } catch (error) {
-          console.log(error);
-          setIsLoading(false);
-        }
-      };
-    
-      const handleDeleteRecord = async (id) => {
-        try {
-          const response = await deleteRecord(id);
-    
-          if (response?.acknowledged) {
-            const newData = data.filter((el) => el._id !== id);
-    
-            setData(newData);
-          }
-        } catch (error) {
-          console.log(error);
-        }
-      };
-    
-      const handleEditRecord = (id) => {
-        router.push(`/edit?id=${id}`);
+  const fetchRecords = async () => {
+    try {
+      const response = await getRecords();
+
+      setRecords(response);
+      setIsLoading(false);
+    } catch (error) {
+      console.error(error);
+      setIsLoading(false);
+    }
+  };
+
+  const handleDeleteRecord = async (id) => {
+    try {
+      const response = await deleteRecord(id);
+
+      if (response.deletedCount === 1) {
+        const newRecords = records.filter((record) => record._id !== id);
+        setRecords(newRecords);
       }
-    
+    } catch (error) {
+      console.log(error)
+    }
+  };
+     
       useEffect(() => {
         fetchRecords();
       }, []);
@@ -47,7 +42,7 @@ const MainPage = () => {
 
     return (
         <div className="p-4 flex flex-wrap gap-4">
-      {data?.map((record) => (
+      {records.map((record) => (
         <div
           key={record._id}
           className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
@@ -58,20 +53,15 @@ const MainPage = () => {
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
             {record.description}
           </p>
+          
           <button
-            type="button"
-            onClick={() => handleEditRecord(record._id)}
-            className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-          >
-            Update
-          </button>
-          <button
-            type="button"
-            onClick={() => handleDeleteRecord(record._id)}
-            className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-          >
-            Delete
-          </button>
+          type="button"
+          className="text-white bg-black hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300
+          dark:focus:ring-gray-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+          onClick={() => handleDeleteRecord(record._id)}
+        >
+          Delete
+        </button>
         </div>
       ))}
     </div>
